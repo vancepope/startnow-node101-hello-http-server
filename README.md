f0871b957773ab11680cd1e20719ebafcb1eb542
Vance Pope <vancepope@outlook.com>
2018-08-30 10:14:36 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# startnow-node101-hello-http-server
---
+++ index.js
@@ -0,0 +1,6 @@
+// write your code here
+var http = require('http');
+http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('Hello World');
+}).listen(8080);
